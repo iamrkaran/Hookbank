210027e03ab112d909b744b02a4f6d7bfb942baf
iamClutchX <iamr.karan@gmail.com>
2022-09-12 17:15:21 +0530

    SignUp fixed

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -141,20 +141,7 @@ const Button = (styles) => {
                 </form>
                 {/*footer*/}
                 <div className="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b">
-                  <button
-                    className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-                    type="button"
-                    onClick={() => setShowModal(false)}
-                  >
-                    Close
-                  </button>
-                  <button
-                    className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-                    type="button"
-                    onClick={() => setShowModal(false)}
-                  >
-                    Save Changes
-                  </button>
+                  
                 </div>
               </div>
             </div>
